f4b5f0d0d82905db4db9c3e53f80ae91c3718942
Philippe16 <philippe.suenson@hotmail.com>
2022-06-13 08:19:37 +0200

    added Festival and Guest entity

--- src/main/java/entities/Festival.java
+++ src/main/java/entities/Festival.java
@@ -1,9 +1,79 @@
 package entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
 @Table(name = "festival")
 public class Festival {
+
+    @Id
+    @Basic(optional = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "festival_id", nullable = false, unique = true)
+    private int festivalID;
+
+    @Basic(optional = false)
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Basic(optional = false)
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @Basic(optional = false)
+    @Column(name = "startDate", nullable = false)
+    private LocalDate startDate;
+
+    @Basic(optional = false)
+    @Column(name = "duration", nullable = false)
+    private LocalTime duration;
+
+
+    public Festival() {
+    }
+
+    public Festival(String name, String city, LocalDate startDate, LocalTime duration) {
+        this.name = name;
+        this.city = city;
+        this.startDate = startDate;
+        this.duration = duration;
+    }
+
+    public int getFestivalID() {
+        return festivalID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalTime getDuration() {
+        return duration;
+    }
+
+    public void setDuration(LocalTime duration) {
+        this.duration = duration;
+    }
 }
--- src/main/java/entities/Guest.java
+++ src/main/java/entities/Guest.java
@@ -1,9 +1,71 @@
 package entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
 @Table(name = "guest")
 public class Guest {
+
+    @Id
+    @Basic(optional = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "guest_id", nullable = false, unique = true)
+    private int guestID;
+
+    @Basic(optional = false)
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Basic(optional = false)
+    @Column(name = "phone", nullable = false)
+    private String phone;
+
+    @Basic(optional = false)
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Basic(optional = false)
+    @Column(name = "status", nullable = false)
+    private String status;
+
+    public Guest() {
+    }
+
+    public int getGuestID() {
+        return guestID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
