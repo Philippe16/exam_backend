c65d029157580eb1e9a2150cebac7f591672d0ee
Philippe16 <philippe.suenson@hotmail.com>
2022-06-13 08:11:17 +0200

    added show entity

---
+++ src/main/java/entities/Show.java
@@ -0,0 +1,94 @@
+package entities;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "show")
+public class Show {
+
+
+    @Id
+    @Basic(optional = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "show_id", nullable = false, unique = true)
+    private int showID;
+
+    @Basic(optional = false)
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Basic(optional = false)
+    @Column(name = "duration", nullable = false)
+    private int duration;
+
+    @Basic(optional = false)
+    @Column(name = "location", nullable = false)
+    private String location;
+
+    @Basic(optional = false)
+    @Column(name = "startDate", nullable = false)
+    private LocalDate startDate;
+
+    @Basic(optional = false)
+    @Column(name = "startTime", nullable = false)
+    private LocalTime startTime;
+
+    public Show() {
+    }
+
+    public Show(String name, int duration, String location, LocalDate startDate, LocalTime startTime) {
+        this.name = name;
+        this.duration = duration;
+        this.location = location;
+        this.startDate = startDate;
+        this.startTime = startTime;
+    }
+
+    public int getShowID() {
+        return showID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+}
